386dde28c8013051c5973a92e75479dc32e16393
sir.woody <sir.woody@theknightsofunity.com>
2025-04-19 20:29:40 +0200

    Added file selection.

--- photo/main.js
+++ photo/main.js
@@ -1,4 +1,6 @@
 var sendButton = document.getElementById("sendButton")
+var filesLabel = document.getElementById("filesLabel")
+var filesInput = document.getElementById("filesInput")
 
 sendButton.addEventListener("pointerdown", function(e) {
     console.log("Test")
